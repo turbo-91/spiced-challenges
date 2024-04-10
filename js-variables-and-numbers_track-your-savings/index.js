bf774bc0eaade8a9480ccca8498f81b2bda3f8fd
Torben Jost <torbenjost@proton.me>
2024-04-10 12:22:45 +0200

    write track savings program

---
+++ js-variables-and-numbers_track-your-savings/index.js
@@ -0,0 +1,10 @@
+startingBalance = 1000;
+monthlyContribution = 250;
+months = 12;
+
+totalSavings = startingBalance + monthlyContribution * months;
+console.log(
+  "After 12 months of saving, you will have",
+  totalSavings,
+  "€ in your account."
+);
