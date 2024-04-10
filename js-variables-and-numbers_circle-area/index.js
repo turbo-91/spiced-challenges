c24cd19fa80b820d291a598a0f5014be4625fcd2
Torben Jost <torbenjost@proton.me>
2024-04-10 13:29:42 +0200

    write program to Calculate Circle Area and Circumference

---
+++ js-variables-and-numbers_circle-area/index.js
@@ -0,0 +1,13 @@
+radiusOfACircle = 25091991;
+circleArea = Math.PI * Math.pow(radiusOfACircle, 2);
+circumference = 2 * Math.PI * radiusOfACircle;
+
+console.log(
+  "For a circle with a radius of",
+  radiusOfACircle,
+  "units we can say that the area is approximately",
+  circleArea,
+  "square units and that the circumference is approximately",
+  circumference,
+  "units."
+);
