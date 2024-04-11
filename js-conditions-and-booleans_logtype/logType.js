56edcd2505aac65aaf420ac9cc9414d67a071580
Torben Jost <torbenjost@proton.me>
2024-04-11 16:19:07 +0200

    write switch statement

--- js-conditions-and-booleans_logtype/logType.js
+++ js-conditions-and-booleans_logtype/logType.js
@@ -1,58 +1,31 @@
-const data = [
-  ,
-  null,
-  5,
-  isNaN("Hello!"),
-  "My backshot goes like bongos!",
-  true,
-  9007199254740991n,
-  function printLetter(name) {
-    console.log("Hi " + name + ", hope you are fine. Love, Johnny");
-  },
-  {
-    name: "Max Paddington",
-    age: 21,
-    isStudent: false,
-  },
-  ["apple", "tomato"],
-  (type = "I have no idea!"),
-];
+let data;
 
-switch (data) {
-    case "undefined!":
-      console.log(typeof ,);
-      break;
-    case "null!":
-      console.log(typeof null);
-      break;
-    case "number!":
-      console.log(typeof 5);
-      break;
-    case "not a number!":
-      console.log(typeof isNaN("Hello!"));
-      break;
-      case "boolean!":
-      console.log(typeof true);
-      break;
-    case "bigint!":
-      console.log(typeof 9007199254740991n);
-      break;
-    case "function!":
-      console.log(typeof function printLetter(name) {
-        console.log("Hi " + name + ", hope you are fine. Love, Johnny");
-      });
-      break;
-    case "object!":
-      console.log(typeof {
-        name: "Max Paddington",
-        age: 21,
-        isStudent: false,
-      });
-      break;
-    case "array!":
-      console.log(typeof ["apple", "tomato"]);
-      break;
-    case "I have no idea!":
-      console.log(typeof (type = "I have no idea!"));
-      break;
-  }
+switch (typeof data) {
+  case "undefined":
+    console.log("undefined!");
+    break;
+  case "null":
+    console.log("null!");
+  case "NaN":
+    console.log("not a number!");
+  case "string":
+    console.log("string!");
+    break;
+  case "boolean":
+    console.log("boolean!");
+    break;
+  case "bigint":
+    console.log("bigint!");
+    break;
+  case "function":
+    console.log("function!");
+    break;
+  case "object":
+    console.log("object!");
+    break;
+  case "array":
+    console.log("array!");
+    break;
+  default:
+    console.log("I have no idea!");
+}
