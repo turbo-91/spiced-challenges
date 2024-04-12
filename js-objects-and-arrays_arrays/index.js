aaf5917a301a4b79df4feea4678b43c83e2dbb24
Torben Jost <torbenjost@proton.me>
2024-04-12 10:42:28 +0200

    solve exercise 1

--- js-objects-and-arrays_arrays/index.js
+++ js-objects-and-arrays_arrays/index.js
@@ -4,6 +4,9 @@ console.clear();
 // Modify the array exampleArray so that it contains a number and a string.
 
 const exampleArray = [true];
+exampleArray[0] = 56;
+exampleArray[exampleArray.length] = "I hate Math!";
+console.log("The result to EXERCISE 1 is:", exampleArray);
 
 // EXERCISE 2
 // Nest an array inside array nestedArray. After that nestedArray should contain an array as an element.
