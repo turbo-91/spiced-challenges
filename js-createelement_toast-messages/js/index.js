ab6ec4713c8895b29041903a55720a341f15a2e2
Torben Jost <torbenjost@proton.me>
2024-04-17 13:01:30 +0200

    js-createelement_toast-messages solve challenge

--- js-createelement_toast-messages/js/index.js
+++ js-createelement_toast-messages/js/index.js
@@ -4,13 +4,19 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-const toastMessage = document.createElement("list");
-console.log(toastMessage);
+/* 
+const toastMessageContainer = document.createElement("list");
+console.log(toastMessageContainer); */
 
-addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+const newToastContainer = document.createElement("list");
+
+addButton.addEventListener("click", (e) => {
+  e.preventDefault;
+  toastContainer.append(newToastContainer);
+  newToastContainer.textContent = "I'm also a toast message.";
+  newToastContainer.classList.add("toast-container__message");
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
