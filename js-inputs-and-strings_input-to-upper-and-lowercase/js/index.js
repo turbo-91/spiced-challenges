e8f4666dafed7a98baa3705888fcc87fb9da1f14
Torben Jost <torbenjost@proton.me>
2024-04-17 07:58:56 +0200

    assign variable to second button

--- js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
+++ js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
@@ -14,6 +14,7 @@ Hint: Access the value by using `.value` on the input element
 
 const input = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
 
 uppercaseButton.addEventListener("click", () => {
   input.value = input.value.toUpperCase();
