2044f172b45d88c08f4259ca40fb9c3057cd3847
Torben Jost <torbenjost@proton.me>
2024-04-17 12:15:04 +0200

    create list element for toast messages

--- js-createelement_toast-messages/js/index.js
+++ js-createelement_toast-messages/js/index.js
@@ -4,6 +4,9 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
+const toastMessage = document.createElement("list");
+console.log(toastMessage);
+
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
 });
