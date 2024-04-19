d3e40f25a35cdbc133cff02899537f8cbb6802ed
Torben Jost <torbenjost@proton.me>
2024-04-19 13:21:05 +0200

    generate DOM elements for a question card

--- recap-project-2_quiz-app/js/form.js
+++ recap-project-2_quiz-app/js/form.js
@@ -6,10 +6,16 @@ form.addEventListener("submit", (event) => {
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
-  const paragraph = document.createElement("paragraph");
-  paragraph.textContent = "test!";
-  document.body.append(paragraph);
-});
+  // how do I get the Form Date into the Card? Really FormData and then some kinda loop? Or do I grab every individual one with target element?
+  // Check in the code from class and the exercises of the others and come up with (a probably failing) attempt, then ask coach.
 
-//const section = document.createElement("section");
-//const img = document.createElement("img");
+  const sectionCard = document.createElement("section");
+  const img = document.createElement("img");
+  const subheadling = document.createElement("h2");
+  const button = document.createElement("button");
+  const paragraph = document.createElement("paragraph");
+  const divTagContainer = document.createElement("div");
+  const divTag = document.createElement("div");
+
+  // appends will go here. Is this also all singularly? Or some kind of Loop sitation?
+});
