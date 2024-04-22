e135b40932b116ad1e1d124943cc3b3aa2a8aab5
Torben Jost <torbenjost@proton.me>
2024-04-22 10:10:34 +0200

    write function to show / hide button

--- recap-project-2_quiz-app/js/script.js
+++ recap-project-2_quiz-app/js/script.js
@@ -6,22 +6,20 @@ bookmark.addEventListener("click", () => {
   bookmark.classList.toggle("question-card__icon__hidden");
 });
 
-// button text toggle
+// answer toggle
 
 const button = document.querySelector('[data-js="button"]');
 const answer = document.querySelector('[data-js="answer"]');
-
-function buttonSwitch() {
-  if (button.getAttribute === "question-card__answer__unhide") {
-    button.textContent = "Hide answer";
-  } else {
-    button.textContent = "show answer";
-  }
-}
 
 button.addEventListener("click", () => {
   answer.classList.toggle("question-card__answer__unhide");
   buttonSwitch();
 });
 
-// show / hide answer toggle
+function buttonSwitch() {
+  if (button.textContent === "hide answer") {
+    button.textContent = "show answer";
+  } else {
+    button.textContent = "hide answer";
+  }
+}
