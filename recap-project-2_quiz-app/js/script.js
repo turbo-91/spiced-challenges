4a517d14cb707e9362f1bd0613063482533f285c
Torben Jost <torbenjost@proton.me>
2024-04-22 08:19:29 +0200

    style elements

--- recap-project-2_quiz-app/js/script.js
+++ recap-project-2_quiz-app/js/script.js
@@ -6,12 +6,22 @@ bookmark.addEventListener("click", () => {
   bookmark.classList.toggle("question-card__icon__hidden");
 });
 
-// show / hide answer toggle
+// button text toggle
 
 const button = document.querySelector('[data-js="button"]');
 const answer = document.querySelector('[data-js="answer"]');
 
+function buttonSwitch() {
+  if (button.getAttribute === "question-card__answer__unhide") {
+    button.textContent = "Hide answer";
+  } else {
+    button.textContent = "show answer";
+  }
+}
+
 button.addEventListener("click", () => {
-  button.classList.toggle("question-card__button__switch");
   answer.classList.toggle("question-card__answer__unhide");
+  buttonSwitch();
 });
+
+// show / hide answer toggle
