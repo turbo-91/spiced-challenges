822ddd5953a664720421a7c6884fdec94e147ff9
Torben Jost <torbenjost@proton.me>
2024-04-26 12:04:57 +0200

    logged some fun stuff hihi

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -1,13 +1,29 @@
 console.clear();
 
-const url = "https://swapi.dev/api/people";
+const url = "https://swapi.py4e.com/api/people";
 
 async function fetchData() {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      console.error("Bad Romance");
+    }
+  } catch (error) {
+    console.error("Someone fucked up");
+  }
 }
 
-fetchData();
+const dataObject = await fetchData();
 
-console.log(data);
+console.log("Entire object: ", dataObject.results);
+
+console.log("Princess with iconic donut hair: ", dataObject.results[4].name);
+console.log("R2D2 has " + dataObject.results[2].eye_color + " eyes.");
+console.log(
+  "Slavery will always be bad even if you do it to Androids whose skin color is",
+  dataObject.results[1].skin_color
+);
