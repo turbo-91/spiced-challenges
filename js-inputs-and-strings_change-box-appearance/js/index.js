5df91d8e9787885344e366e543dde12cf673c06c
Torben Jost <torbenjost@proton.me>
2024-05-04 17:23:39 +0200

    solve js-inputs-and-strings_change-box-appearance

--- js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings_change-box-appearance/js/index.js
@@ -3,9 +3,12 @@
 Change the appearance of the box according to the values of the sliders (color, border radius, rotation)
 
 – [ ] Add an event listener for each input element.
-– [ ] When the slider control of inputColor is moved, change the background color of the box. Hint: You need to work with the hsl() functional notation for colors.
-– [ ] When the slider control of inputRadius is moved, change the border radius of the box. If the control reaches the right end, make the box look like a circle.
-– [ ] When the slider control of inputRotation is moved, rotate the box. Hint: You may need to work with the CSS property "transform" and a value that contains "rotate" and "deg".
+– [ ] When the slider control of inputColor is moved, change the background color of the box. 
+Hint: You need to work with the hsl() functional notation for colors.
+– [ ] When the slider control of inputRadius is moved, change the border radius of the box. 
+If the control reaches the right end, make the box look like a circle.
+– [ ] When the slider control of inputRotation is moved, rotate the box. 
+Hint: You may need to work with the CSS property "transform" and a value that contains "rotate" and "deg".
 
 Hint: You can work with the property "style", just like you worked with the property "classList".
 
@@ -15,4 +18,25 @@ For further information check MDN:
 - about transform & rotate: https://developer.mozilla.org/en-US/docs/Web/CSS/transform
 - about CSS declarations with "style": https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
 
+
 */
+
+const colorInput = document.querySelector('[data-js="input-color"]');
+const borderInput = document.querySelector('[data-js="input-radius"]');
+const rotationInput = document.querySelector('[data-js="input-rotation"]');
+const box = document.querySelector('[data-js="box"]');
+
+colorInput.addEventListener("input", () => {
+  const hue = colorInput.value;
+  const saturation = colorInput.value;
+  const lightness = 90;
+  box.style.background = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+});
+
+borderInput.addEventListener("input", () => {
+  box.style.borderRadius = `${borderInput.value}%`;
+});
+
+rotationInput.addEventListener("input", () => {
+  box.style.transform = "rotateZ(" + rotationInput.value + "deg)";
+});
