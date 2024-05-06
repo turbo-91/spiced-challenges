1263e652a2354a9f8d097e58497798d2dcb0c895
Torben Jost <torbenjost@proton.me>
2024-05-06 18:00:17 +0200

    solve react-nesting_fragments challenge

--- react-nesting_fragments/src/App.jsx
+++ react-nesting_fragments/src/App.jsx
@@ -2,11 +2,17 @@ import "./App.css";
 import Box from "./components/Box";
 
 export default function App() {
+  return <Boxes />;
+}
+
+const Boxes = () => {
   return (
     <main className="flex-container">
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
+      <>
+        <Box color="#007bff" />
+        <Box color="#fc3" />
+        <Box color="#ff3333" />
+      </>
     </main>
   );
-}
+};
