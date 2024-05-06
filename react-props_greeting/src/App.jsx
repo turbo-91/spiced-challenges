6b6db804822b60357f0e2226b502986a0f898b63
Torben Jost <torbenjost@proton.me>
2024-05-06 12:37:58 +0200

    solve react-props_greeting challenge

--- react-props_greeting/src/App.jsx
+++ react-props_greeting/src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting />;
 }
+
+const Greeting = ({ name }) => {
+  if ((name = "Gimena")) return <p>Hello, Coach!</p>;
+  return <p>Hello, {name}!</p>;
+};
