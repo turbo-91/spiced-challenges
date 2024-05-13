72e15ff0f5b4eec4b9d87b1cd82542421fe14310
Torben Jost <torbenjost@proton.me>
2024-05-13 11:24:48 +0200

    import uid package

--- react-state-3_adding-animals/src/App.jsx
+++ react-state-3_adding-animals/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Form from "./components/Form";
 import List from "./components/List";
 import "./App.css";
+import { uid } from "uid";
 
 const initialAnimals = [
   {
