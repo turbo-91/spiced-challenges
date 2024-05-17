a0932e19176d204aa79db1f7d27a86dbc2b46936
Torben Jost <torbenjost@proton.me>
2024-05-17 14:55:39 +0200

    add volume 1 page content

--- lotr-app/src/pages/volumes/the-fellowship-of-the-ring.js
+++ lotr-app/src/pages/volumes/the-fellowship-of-the-ring.js
@@ -1,5 +1,25 @@
+import Link from "next/link";
 import React from "react";
+import { volumes } from "@/resources/lib/data";
 
 export default function TheFellowshipOfTheRing() {
-  return <div>the-fellowship-of-the-ring</div>;
+  let fellowshipSlug = volumes.find(
+    ({ slug }) => slug === "the-fellowship-of-the-ring"
+  );
+  console.log(fellowshipSlug);
+  return (
+    <>
+      <Link href={`/volumes/`}>All Volumes</Link>
+      <h1>{fellowshipSlug.title}</h1>
+      <p>{fellowshipSlug.description}</p>
+      <ul>
+        <li>
+          {`${fellowshipSlug.books[0].ordinal}: "${fellowshipSlug.books[0].title}"`}
+        </li>
+        <li>
+          {`${fellowshipSlug.books[1].ordinal}: "${fellowshipSlug.books[1].title}"`}
+        </li>
+      </ul>
+    </>
+  );
 }
