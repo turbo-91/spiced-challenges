390755e2039920b76bfefd3cbda8ccb24c292bbb
Torben Jost <torbenjost@proton.me>
2024-05-17 14:59:52 +0200

    add volume 1 & volume 2 pages content

--- lotr-app/src/pages/volumes/the-return-of-the-king.js
+++ lotr-app/src/pages/volumes/the-return-of-the-king.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
 import React from "react";
+import { volumes } from "@/resources/lib/data";
 
 export default function TheReturnOfTheKing() {
-  return <div>the-return-of-the-king</div>;
+  let returnKingSlug = volumes.find(
+    ({ slug }) => slug === "the-return-of-the-king"
+  );
+  return (
+    <>
+      <Link href={`/volumes/`}>All Volumes</Link>
+      <h1>{returnKingSlug.title}</h1>
+      <p>{returnKingSlug.description}</p>
+      <ul>
+        <li>
+          {`${returnKingSlug.books[0].ordinal}: "${returnKingSlug.books[0].title}"`}
+        </li>
+        <li>
+          {`${returnKingSlug.books[1].ordinal}: "${returnKingSlug.books[1].title}"`}
+        </li>
+      </ul>
+    </>
+  );
 }
--- lotr-app/src/pages/volumes/the-two-towers.js
+++ lotr-app/src/pages/volumes/the-two-towers.js
@@ -1,5 +1,22 @@
+import Link from "next/link";
 import React from "react";
+import { volumes } from "@/resources/lib/data";
 
 export default function TheTwoTowers() {
-  return <div>the-two-towers</div>;
+  let towersSlug = volumes.find(({ slug }) => slug === "the-two-towers");
+  return (
+    <>
+      <Link href={`/volumes/`}>All Volumes</Link>
+      <h1>{towersSlug.title}</h1>
+      <p>{towersSlug.description}</p>
+      <ul>
+        <li>
+          {`${towersSlug.books[0].ordinal}: "${towersSlug.books[0].title}"`}
+        </li>
+        <li>
+          {`${towersSlug.books[1].ordinal}: "${towersSlug.books[1].title}"`}
+        </li>
+      </ul>
+    </>
+  );
 }
