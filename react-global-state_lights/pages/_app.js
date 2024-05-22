3fbf29b8634faf690ee42e5bcea9d4c6eb31fd97
Torben Jost <torbenjost@proton.me>
2024-05-22 15:48:22 +0200

    lift state up to _app.js

--- react-global-state_lights/pages/_app.js
+++ react-global-state_lights/pages/_app.js
@@ -1,7 +1,21 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
+
+const initialLights = [
+  { id: 1, name: "Living Room", isOn: false },
+  { id: 2, name: "Kitchen", isOn: false },
+  { id: 3, name: "Bedroom", isOn: true },
+  { id: 4, name: "Bathroom", isOn: false },
+  { id: 5, name: "Garage", isOn: false },
+  { id: 6, name: "Porch", isOn: false },
+  { id: 7, name: "Garden", isOn: false },
+  { id: 8, name: "Office", isOn: false },
+];
 
 export default function App({ Component, pageProps }) {
+  const [lights, setLights] = useState(initialLights);
+
   return (
     <Layout>
       <GlobalStyle />
