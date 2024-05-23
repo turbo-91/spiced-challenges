43b126bd02281cd10fdc299fd3b09180eb573261
Torben Jost <torbenjost@proton.me>
2024-05-23 14:43:40 +0200

    fetch Luke Skywalker

--- react-data-fetching_star-wars/pages/characters/1.js
+++ react-data-fetching_star-wars/pages/characters/1.js
@@ -1,17 +1,37 @@
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
+import useSWR from "swr";
+
+const URL = "https://swapi.py4e.com/api/people/1";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  // If the status code between 200-299 still try to parse and throw
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+  return res.json();
+};
 
 export default function Character() {
   const id = 1;
+  const { data, error, isLoading } = useSWR(URL, fetcher, {});
+  console.log("data", data);
+  if (error) return <div>An error has occurred...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <Layout>
       <Card
         id={id}
-        name={"Luke Skywalker"}
-        height={172}
-        eyeColor={"blue"}
-        birthYear={"19BBY"}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
       />
     </Layout>
   );
