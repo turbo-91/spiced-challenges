d740334a5c9ab31999508ebfac8b56f0a731e3ba
Torben Jost <torbenjost@proton.me>
2024-06-12 10:37:31 +0200

    dereks solution

--- kata-challenges/kata_12-06-24/index.js
+++ kata-challenges/kata_12-06-24/index.js
@@ -1,11 +1,103 @@
-// I need to write a function boredom(staff)
-// that access the value of every property of the object staff
-// and assigns it the corresponding value of the boredomAssessmentScore
-// these values need to be sumed up
-// the I need to determine (function! condition!) where this sum falls on the appropriateSentiment scale
-// and return the corresponding string
+// Assess the team to determine the boredom level of the team
 
-let staff = {
+// Provided: object(staff) with names as keys and department as values.
+
+// boredom scores:
+// accounts = 1
+// finance = 2
+// canteen = 10
+// regulation = 3
+// trading = 6
+// change = 6
+// IS = 8
+// retail = 5
+// cleaning = 4
+// pissing about = 25
+
+// Depending on the cumulative score of the team, return the appropriate sentiment:
+
+// <=80: 'kill me now'
+// < 100 & > 80: 'i can handle this'
+// 100 or over: 'party time!!'
+
+// What we need:
+// - Iterate through each employee to determine their department
+// - Increase the team score by the department's boredom score
+// - Return the appropriate sentiment based on the total team score
+
+// the correct way
+function boredom(staff) {
+  let teamScore = 0;
+  let response = "";
+
+  for (const employee in staff) {
+    switch (staff[employee]) {
+      case "accounts":
+        teamScore += 1;
+        break;
+      case "finance":
+        teamScore += 2;
+        break;
+      case "canteen":
+        teamScore += 10;
+        break;
+      case "regulation":
+        teamScore += 3;
+        break;
+      case "trading":
+        teamScore += 6;
+        break;
+      case "change":
+        teamScore += 6;
+        break;
+      case "IS":
+        teamScore += 8;
+        break;
+      case "retail":
+        teamScore += 5;
+        break;
+      case "cleaning":
+        teamScore += 4;
+        break;
+      case "pissing about":
+        teamScore += 25;
+        break;
+      default:
+        break;
+    }
+  }
+  teamScore <= 80 ? (response = "kill me now") : "";
+  teamScore < 100 && teamScore > 80 ? (response = "i can handle this") : "";
+  teamScore >= 100 ? (response = "party time!!") : "";
+  return response;
+}
+
+// the confusing way
+function boredom1(staff) {
+  let totalScore = 0;
+  const deptScore = {
+    accounts: 1,
+    finance: 2,
+    canteen: 10,
+    regulation: 3,
+    trading: 6,
+    change: 6,
+    IS: 8,
+    retail: 5,
+    cleaning: 4,
+    "pissing about": 25,
+  };
+  for (const employee in staff) {
+    console.log("staff[employee]", staff[employee]);
+    console.log(
+      "department score staff employee: ",
+      deptScore[staff[employee]]
+    );
+    totalScore += deptScore[staff[employee]];
+  }
+  //   console.log("Total Score: ", totalScore);
+}
+boredom1({
   tim: "change",
   jim: "accounts",
   randy: "canteen",
@@ -17,28 +109,4 @@ let staff = {
   alex: "trading",
   john: "accounts",
   mr: "finance",
-};
-
-const boredomAssessmentScore = {
-  accounts: 1,
-  finance: 2,
-  canteen: 10,
-  regulation: 3,
-  trading: 6,
-  change: 6,
-  IS: 8,
-  retail: 5,
-  cleaning: 4,
-  pissingAbout: 25,
-};
-
-function boredom(staff) {
-  let totalScore = 0;
-  for (const key in staff) {
-    staff.value === "the corresponding key.value of the boredomAssessmentScore"; // could be done with if/else
-    totalScore += staff[key];
-    if (totalScore < 80) return console.log("kill me now");
-    if (totalScore < 100 && sum > 80) return console.log("I can handle this");
-    else return console.log("party time!!");
-  }
-}
+});
