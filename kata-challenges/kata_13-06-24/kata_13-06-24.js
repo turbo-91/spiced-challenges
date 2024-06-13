6e4fa8c197dc0d0649b12c9f52a6ac492e139243
Torben Jost <torbenjost@proton.me>
2024-06-13 09:48:02 +0200

    paraphrase instructions

---
+++ kata-challenges/kata_13-06-24/kata_13-06-24.js
@@ -0,0 +1,5 @@
+// I need to write a function sabb
+// that counts how often the letters of the word 'sabbatical'
+// exist in the provided String. Then I need to store that number in a
+// variable, and sum it with the respective variables for val and happiness.
+// If the sum is > 22 I return 'Sabbatical! Boom!', else 'Back to your desk, boy.'
