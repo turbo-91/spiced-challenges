c31760b33f753ee501c44bfe9b365b2767f01303
Torben Jost <torbenjost@proton.me>
2024-06-13 10:19:50 +0200

    solve kata

--- kata-challenges/kata_13-06-24/kata_13-06-24.js
+++ kata-challenges/kata_13-06-24/kata_13-06-24.js
@@ -3,3 +3,30 @@
 // exist in the provided String. Then I need to store that number in a
 // variable, and sum it with the respective variables for val and happiness.
 // If the sum is > 22 I return 'Sabbatical! Boom!', else 'Back to your desk, boy.'
+
+let string =
+  "GIVE ME THE FUCKING SABBATICAL RIGHT THE FUCK NOW OR I WILL KILL YOU!!!!!!!!!!";
+let val = 0;
+let happiness = 0;
+
+function sabb(string, val, happiness) {
+  function countLetters(string) {
+    const lowerString = string.toLowerCase();
+    const sabbatical = "sabbatical";
+    let count = 0;
+    for (let letter of sabbatical) {
+      count += lowerString.split("").filter((char) => char === letter).length;
+    }
+    return count;
+  }
+
+  const count = countLetters(string); // Correctly store the result in `count`
+  let finalSum = count + val + happiness;
+  if (finalSum > 22) {
+    return "Sabbatical! Boom!";
+  } else {
+    return "Back to your desk, boy.";
+  }
+}
+
+console.log(sabb(string, val, happiness));
